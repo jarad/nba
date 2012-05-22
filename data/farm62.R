55262446e2538b55074c8fd181982bae8b8f77de
Jarad Niemi <niemi@iastate.edu>
2012-05-22 15:39:45 -0500

    switching to desktop

--- data/farm62.R
+++ data/farm62.R
@@ -1,37 +1,36 @@
-# Pedigree random effects
-# manually create pedigree
-n.sire = 10
-n.dam.only = 20
-sire = rep(1:n.sire,each=n.dam.only)
-dam  = rep(n.sire+(1:n.dam.only),n.sire)
-id   = seq(from=n.sire+n.dam.only+1,length=length(dam),by=1)
-maxparity = sample(4, length(id), replace=T)
-n    = sum(maxparity)
+d = read.table("farm62.txt", header=T)
 
-a = rnorm(n.sire+n.dam.only,0,1)
-for (i in 1:length(id)) a[id[i]] = rnorm(1,.5*a[sire[i]]+.5*a[dam[i]],.1)
+sires = unique(d$sire); n.sires = length(sires)
+dams  = unique(d$dam)
+sows = unique(d$sowid)
+dams.only = setdiff(dams,sows); n.dams.only = length(dams.only)
+other.sows = setdiff(sows,dams.only)
 
-# Fixed effects
-id = rep(id,maxparity)
-x = matrix(rnorm(n),ncol=1)
-b = .1
+ids = factor(c(sires,dams,other.sows),c(sires,dams,other.sows), ordered=T)
 
-# Response
-y = rpois(n,exp(2.5+x%*%b+a[id]))
-hist(y,seq(-.5,max(y)+.5,by=1))
 
-# Data frame
-d = data.frame(y=y, id=id, x=x,
-               sire = rep(sire, maxparity),
-               dam  = rep(dam , maxparity))
+# Pedigree
+first = match(sows,d$sowid)  # Assumes all the data are correct
+pedigree = d[first,c(1,6,7)]
+pedigree$sire = 
 
 # Mixed effect Poisson regression (no pedigree)
 require(lme4)
-summary(freq <- glmer(y~x+(1|id), family=poisson))
-#plot(a[-c(1:30)],ranef(freq)$id[,1])
+summary(freq <- glmer(nba~factor(parity)+(1|sowid), d, family=poisson))
+re = ranef(freq)$sowid[,1]
+
+par(mfrow=c(1,2))
+hist(re, freq=F, 30)
+curve(dnorm(x, mean(re), sd(re)), add=T)
+qqnorm(re)
+qqline(re)
+
 
 # Data/inits/model for BUGS/JAGS
-dat = list(n=n,y=y,x=cbind(1,x),p=ncol(x)+1,m=length(a),id=id, sire=sire, dam=dam, 
+x = model.matrix(freq)
+dat = list(n=nrow(d),y=d$nba,x=x,p=ncol(x),
+           m=length(a),
+           id=id, sire=sire, dam=dam, 
            n.sire=length(unique(sire)), n.dam.only=length(unique(dam)))
 parms = c("b","a","sigma")
 model.file= "poisHier.txt"
