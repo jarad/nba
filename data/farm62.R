c98a0557874b9b7ad578410053eb3bc60582af3c
Jarad Niemi <niemi@iastate.edu>
2012-05-23 10:05:37 -0500

    minor edits

--- data/farm62.R
+++ data/farm62.R
@@ -40,12 +40,28 @@ model.file= "../code/poisHier.txt"
 # JAGS
 require(rjags)
 mod = jags.model(model.file, data=dat, n.chains=3, n.adapt=1e4)
-samps = coda.samples(mod, parms, 1e4, thin=10)
+samps = coda.samples(mod, parms, 1e5, thin=100)
 gelman.diag(samps)
 summary(samps)
 
-a.med = apply(as.matrix(samps[,1:230]),2,median)
-plot(a,a.med)
+betas = samps[,dat$m+(1:7)]
+ss = as.matrix(samps)
+sigmas = samps[,ncol(ss)-c(0:2)]
+es = ss[,(n.sires+n.dams.only+1):dat$m]-ss[,1851+((n.sires+n.dams.only+1):dat$m)]
+es.med = apply(es,2,median)
+for (i in 1:nrow(es)) {
+  qqnorm(es[i,])
+  qqline(es[i,])
+  readline("")
+}
+
+summary(betas)
+summary(sigmas)
+
+plot(betas,ask=T)
+
+a.med = apply(as.matrix(samps[,n.sires+n.dams.only+(1:length(re))]),2,median)
+plot(re,a.med)
 text(a[1:30],a.med[1:30],as.character(1:230)[1:30])
 
 
